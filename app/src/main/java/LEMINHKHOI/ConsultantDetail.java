b456e669a058146b938e2c5d09511104cf0c81ba
quanndb <quan2082003@gmail.com>
2023-12-22 12:36:57 +0700

    new

--- app/src/main/java/LEMINHKHOI/ConsultantDetail.java
+++ app/src/main/java/LEMINHKHOI/ConsultantDetail.java
@@ -5,28 +5,65 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import com.example.anassert.CoVanHocTap.CoVanHocTapDAO;
+import com.example.anassert.CoVanHocTap.CoVanHocTapObject;
+import com.example.anassert.CoVanHocTap.HocPhanDto;
 import com.example.anassert.R;
 
+import java.util.ArrayList;
+
 public class ConsultantDetail extends AppCompatActivity {
-   Button btnCheck;
+   Button btnContact;
+   TextView txtName, txtPhone, txtEmail;
+   ListView lv;
+   private CoVanHocTapObject data;
+   private CoVanHocTapDAO _service;
+    ArrayList<HocPhanDto> listData = new ArrayList<>();
+    ArrayAdapter<String> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.consultant_detail);
-        getID();
+        _service = new CoVanHocTapDAO(ConsultantDetail.this);
+        getData();
         app();
     }
-    private void getID(){
-        btnCheck = findViewById(R.id.btnCall);
+    private void getData(){
+        btnContact = findViewById(R.id.btn_contact_messageDetails);
+        txtEmail = findViewById(R.id.txt_email_messageDetails);
+        txtPhone = findViewById(R.id.txt_phone_messageDetails);
+        txtName = findViewById(R.id.txt_fulleName_messageDetails);
+        lv = findViewById(R.id.lv_consultant_details);
+
+        Intent intent = getIntent();
+        if (intent != null) {
+            // Nhận đối tượng từ Intent
+            data = (CoVanHocTapObject) intent.getSerializableExtra("CVHTObject");
+
+            // Sử dụng đối tượng nhận được
+            if (data != null) {
+                txtEmail.setText(data.Email.toString());
+                txtPhone.setText(data.PhoneNumber.toString());
+                txtName.setText(data.FullName.toString());
+
+                listData = _service.getAllHP(data.ID);
+                adapter = new consultant_details_Adapter(ConsultantDetail.this,R.layout.adapter_consultant_details, listData);
+                lv.setAdapter(adapter);
+            }
+        }
     }
     private void app(){
-        btnCheck.setOnClickListener(new View.OnClickListener() {
+        btnContact.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intentConsultantContact = new Intent(ConsultantDetail.this, ConsultantContact.class);
-                startActivity(intentConsultantContact);
+                Intent intentContact = new Intent(ConsultantDetail.this, ConsultantContact.class);
+                intentContact.putExtra("email_contact", data);
+                startActivity(intentContact);
             }
         });
     }
--- app/src/main/java/com/example/anassert/KEHOACH/KeHoachDAO.java
+++ app/src/main/java/com/example/anassert/KEHOACH/KeHoachDAO.java
@@ -7,6 +7,8 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 import com.example.anassert.DBHelper;
+import com.example.anassert.HOCPHAN.HocPhanObject;
+import com.example.anassert.TAIKHOAN.TaiKhoanObject;
 
 import java.util.ArrayList;
 
@@ -44,22 +46,22 @@ public class KeHoachDAO {
     }
 
     @SuppressLint("Range")
-    public KeHoachObject getKH (int userID){
+    public KeHoachObject getKH (int idsv){
         ArrayList<KeHoachObject> listKH = new ArrayList<>();
         // Câu truy vấn kiểm tra xem username có tồn tại không
         String query = "SELECT * FROM tblKH WHERE IDSV = ?";
-        String selectionArgs[] = {userID+""};
+        String selectionArgs[] = {idsv+""};
 
         // Thực hiện truy vấn
         Cursor cursor = db.rawQuery(query,selectionArgs);
 
         if (cursor.moveToFirst()) {
             // Truy cập dữ liệu từ Cursor
-
+            int ID = cursor.getInt(cursor.getColumnIndex("ID"));
             String updateDate = cursor.getString(cursor.getColumnIndex("updateDate"));
             int IDSV = cursor.getInt(cursor.getColumnIndex("IDSV"));
 
-            KeHoachObject newKH = new KeHoachObject(updateDate,IDSV);
+            KeHoachObject newKH = new KeHoachObject(ID,updateDate,IDSV);
             cursor.close();
             return newKH;
         }
