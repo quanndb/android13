6264280608224f25b3531e9b77ce7f85ae30b7a3
XuanDiep <vulq2k3@gmail.com>
2023-12-30 20:16:55 +0700

    update new code

--- app/src/main/java/com/example/anassert/MainActivity.java
+++ app/src/main/java/com/example/anassert/MainActivity.java
@@ -63,6 +63,193 @@ public class MainActivity extends AppCompatActivity {
         getBack();
 
     }
+
+    private void appTeacher() {
+        btnEvaluate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent  intentWarning  = new Intent(MainActivity.this, ReportingAdvisor.class);
+                startActivity(intentWarning);
+            }
+        });
+        btnChat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent emailIntent = new Intent(Intent.ACTION_SENDTO);
+                emailIntent.setData(Uri.parse("mailto:"));
+
+                try {
+                    startActivity(emailIntent);
+                } catch (android.content.ActivityNotFoundException ex) {
+                    // Handle case where no email app is available
+                }
+            }
+        });
+        imageAnother.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Sms();
+            }
+        });
+        imageSugsub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Sms();
+            }
+        });
+
+        logo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentImplicit = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.haui.edu.vn/vn"));
+                startActivity(intentImplicit);
+            }
+        });
+        logo1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentImplicit1 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://fit.haui.edu.vn/vn"));
+                startActivity(intentImplicit1);
+            }
+        });
+    }
+
+    private void appStudent() {
+        //
+        helper = new DBHelper(this);
+        helper.getReadableDatabase();
+        clickInsert();
+        Intent  intent=getIntent();
+        Integer gSV=intent.getIntExtra("idSinhVien",1);
+        String maSV=gSV.toString();
+        clickResult(maSV);
+        //Timetable
+        imageTimetable.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentTimetbable = new Intent(MainActivity.this, Timetable.class);
+                startActivity(intentTimetbable);
+            }
+        });
+        //document
+        imageDocument.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentDocument = new Intent(MainActivity.this, Document.class);
+                startActivity(intentDocument);
+            }
+        });
+        //mesage to teacher
+        imageMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentMesage = new Intent(MainActivity.this, Message.class);
+                startActivity(intentMesage);
+            }
+        });
+       //Study plane
+        imageStudyplane.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentStudyplane = new Intent(MainActivity.this, StudyPlan.class);
+                intentStudyplane.putExtra("IDSV",getSV.getID()+"");
+                startActivity(intentStudyplane);
+            }
+
+        });
+        imageSugsub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentSugsub = new Intent(MainActivity.this, SuggestedSubjects.class);
+                intentSugsub.putExtra("IDSV",getSV.getID()+"");
+                startActivity(intentSugsub);
+            }
+        });
+        imageAnother.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Sms();
+            }
+        });
+        imgReport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Chức năng đang trong quá trình phát triển", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        logo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentImplicit = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.haui.edu.vn/vn"));
+                startActivity(intentImplicit);
+            }
+        });
+        logo1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentImplicit1 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://fit.haui.edu.vn/vn"));
+                startActivity(intentImplicit1);
+            }
+        });
+
+    }
+
+    private void getWidgetStudent() {
+        back = findViewById(R.id.imageback);
+        txtUsername = findViewById(R.id.txtUsername);
+        imageMessage = findViewById(R.id.imageMessage);
+        imageTimetable = findViewById(R.id.imageTimetable);
+        imageDocument = findViewById(R.id.imageDocument);
+        imageResult = findViewById(R.id.imageResult);
+        imageStudyplane = findViewById(R.id.btnStudyplane);
+        imageSugsub = findViewById(R.id.btnSuggestedsubjects);
+        imageAnother = findViewById(R.id.imgAnother);
+        imgReport = findViewById(R.id.imgReport);
+        logo  = findViewById(R.id.logo);
+        logo1  = findViewById(R.id.logo1);
+    }
+
+    private void getWidgetTeacher(){
+        back = findViewById(R.id.imageback);
+        txtUsername = findViewById(R.id.txtUsername);
+        logo  = findViewById(R.id.logo);
+        logo1  = findViewById(R.id.logo1);
+        btnChat = findViewById(R.id.btnChat);
+        btnEvaluate = findViewById(R.id.btnEvaluate);
+        imageSugsub = findViewById(R.id.btnSuggestedsubjects);
+        imageAnother = findViewById(R.id.imgAnother);
+    }
+    private void getBack(){
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder backRequest = new AlertDialog.Builder(MainActivity.this);
+                backRequest.setTitle("Xác nhận đăng xuất");
+                backRequest.setMessage("Bạn có chắc chắn muốn đăng xuất");
+                backRequest.setPositiveButton("Đăng xuất", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+                        Intent backLogin = new Intent(MainActivity.this,Login.class);
+                        startActivity(backLogin);
+                        finish();
+                    }
+                });
+                backRequest.setNegativeButton("Hủy", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+                backRequest.create().show();
+            }
+
+        });
+    }
+    //Thông báo tính năng đang trong quá trình phát triển
+    private void Sms() {
+        Toast.makeText(MainActivity.this, "Tính năng đang trong quá trình phát triển", Toast.LENGTH_SHORT).show();
+    }
     private void clickResult(String sv) {
         imageResult.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -356,189 +543,4 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void appTeacher() {
-        btnEvaluate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent  intentWarning  = new Intent(MainActivity.this, ReportingAdvisor.class);
-                startActivity(intentWarning);
-            }
-        });
-        btnChat.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent emailIntent = new Intent(Intent.ACTION_SENDTO);
-                emailIntent.setData(Uri.parse("mailto:"));
-
-                try {
-                    startActivity(emailIntent);
-                } catch (android.content.ActivityNotFoundException ex) {
-                    // Handle case where no email app is available
-                }
-            }
-        });
-        imageAnother.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Sms();
-            }
-        });
-        imageSugsub.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Sms();
-            }
-        });
-
-        logo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intentImplicit = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.haui.edu.vn/vn"));
-                startActivity(intentImplicit);
-            }
-        });
-        logo1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intentImplicit1 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://fit.haui.edu.vn/vn"));
-                startActivity(intentImplicit1);
-            }
-        });
-    }
-
-    private void appStudent() {
-        //
-        helper = new DBHelper(this);
-        helper.getReadableDatabase();
-        clickInsert();
-        Intent  intent=getIntent();
-        Integer gSV=intent.getIntExtra("idSinhVien",1);
-        String maSV=gSV.toString();
-        clickResult(maSV);
-
-        //Timetable
-        imageTimetable.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intentTimetbable = new Intent(MainActivity.this, Timetable.class);
-                startActivity(intentTimetbable);
-            }
-        });
-        imageDocument.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intentDocument = new Intent(MainActivity.this, Document.class);
-                startActivity(intentDocument);
-            }
-        });
-        imageMessage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intentMesage = new Intent(MainActivity.this, Message.class);
-                startActivity(intentMesage);
-            }
-        });
-
-        imageStudyplane.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intentStudyplane = new Intent(MainActivity.this, StudyPlan.class);
-                intentStudyplane.putExtra("IDSV",getSV.getID()+"");
-                startActivity(intentStudyplane);
-            }
-
-        });
-        imageSugsub.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intentSugsub = new Intent(MainActivity.this, SuggestedSubjects.class);
-                intentSugsub.putExtra("IDSV",getSV.getID()+"");
-                startActivity(intentSugsub);
-            }
-        });
-        imageAnother.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Sms();
-            }
-        });
-        imgReport.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Chức năng đang trong quá trình phát triển", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        logo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intentImplicit = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.haui.edu.vn/vn"));
-                startActivity(intentImplicit);
-            }
-        });
-        logo1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intentImplicit1 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://fit.haui.edu.vn/vn"));
-                startActivity(intentImplicit1);
-            }
-        });
-
-    }
-
-    private void getWidgetStudent() {
-        back = findViewById(R.id.imageback);
-        txtUsername = findViewById(R.id.txtUsername);
-        imageMessage = findViewById(R.id.imageMessage);
-        imageTimetable = findViewById(R.id.imageTimetable);
-        imageDocument = findViewById(R.id.imageDocument);
-        imageResult = findViewById(R.id.imageResult);
-        imageStudyplane = findViewById(R.id.btnStudyplane);
-        imageSugsub = findViewById(R.id.btnSuggestedsubjects);
-        imageAnother = findViewById(R.id.imgAnother);
-        imgReport = findViewById(R.id.imgReport);
-        logo  = findViewById(R.id.logo);
-        logo1  = findViewById(R.id.logo1);
-    }
-
-    private void getWidgetTeacher(){
-        back = findViewById(R.id.imageback);
-        txtUsername = findViewById(R.id.txtUsername);
-        logo  = findViewById(R.id.logo);
-        logo1  = findViewById(R.id.logo1);
-        btnChat = findViewById(R.id.btnChat);
-        btnEvaluate = findViewById(R.id.btnEvaluate);
-        imageSugsub = findViewById(R.id.btnSuggestedsubjects);
-        imageAnother = findViewById(R.id.imgAnother);
-    }
-    private void getBack(){
-        back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AlertDialog.Builder backRequest = new AlertDialog.Builder(MainActivity.this);
-                backRequest.setTitle("Xác nhận đăng xuất");
-                backRequest.setMessage("Bạn có chắc chắn muốn đăng xuất");
-                backRequest.setPositiveButton("Đăng xuất", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int which) {
-                        Intent backLogin = new Intent(MainActivity.this,Login.class);
-                        startActivity(backLogin);
-                        finish();
-                    }
-                });
-                backRequest.setNegativeButton("Hủy", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        dialogInterface.cancel();
-                    }
-                });
-                backRequest.create().show();
-            }
-
-        });
-    }
-    //Thông báo tính năng đang trong quá trình phát triển
-    private void Sms() {
-        Toast.makeText(MainActivity.this, "Tính năng đang trong quá trình phát triển", Toast.LENGTH_SHORT).show();
-    }
 }
